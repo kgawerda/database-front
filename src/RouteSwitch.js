ad1932ae28b16e627e6e248c5eff33ba2892f2e5
kgawerda <konradgawerda@gmail.com>
2023-06-11 20:04:27 +0200

    create basic pages

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import More from "./pages/More";
+import Search from "./pages/Search";
+import Champions from "./pages/Champions";
+import Teams from "./pages/Teams";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/more" element={<More />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/champstat" element={<Champions />} />
+        <Route path="/teamstat" element={<Teams />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link className="nav-link top-left" to="/more">
+              More
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link top-right" to="/search">
+              Search
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link top-center" to="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link bottom-left" to="/champstat">
+              Champion stats
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link bottom-right" to="/teamstat">
+              Team stats
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default NavBar;
