36deba4af897b1e9d21cfa99145746cdfb4f4155
kgawerda <konradgawerda@gmail.com>
2023-06-13 16:15:21 +0200

    test fix api for sides

--- src/pages/More.js
+++ src/pages/More.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 const More = () => {
   const [red, setRed] = useState(null);
   const [blue, setBlue] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const mockData =
     // '{ "data" : [' +
@@ -19,34 +20,44 @@ const More = () => {
     // '{ "championName":"Kwangdong Freecs" , "wr":"23", "br":"11", "pr":"23", "presence":"34"},' +
     // '{ "championName":"Hanwha Life Esports" , "wr":"23", "br":"11", "pr":"23", "presence":"34"},' +
     // '{ "championName":"Fredit BRION" , "wr":"0", "br":"0", "pr":"0", "presence":"0"}]}';
-    `{ "data" : [{ "blue":"40" },{ "red":"60" }]}`;
+    //{ blue: 60, red: 40 };
 
-  useEffect(() => {
-    // const data = JSON.parse(mockData);
-    // setRed(Object.values(data.data[1]));
-    // setBlue(Object.values(data.data[0]));
-    const getData = () => {
-      fetch("http://127.0.0.1:5000/sidewinrate")
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (myJson) {
-          setBlue(Object.values(myJson[0]));
-          setRed(Object.values(myJson[1]));
-        });
-    };
-    getData();
-  }, []);
+    useEffect(() => {
+      // const data = JSON.parse(mockData);
+      // setRed(Object.values(data.data[1]));
+      // setBlue(Object.values(data.data[0]));
+      const getData = () => {
+        fetch("http://127.0.0.1:5000/sidewinrate")
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (myJson) {
+            setBlue(myJson.blue);
+            setRed(myJson.red);
+            setLoading(false);
+          });
+      };
+      setLoading(true);
+      getData();
+    }, []);
 
   return (
     <div className="main">
       <div className="wrapper">
-        <div className="blue" style={{ width: `${blue}vw` }}>
-          <span className="text-container">Blue Side Win Ratio: {blue}%</span>
-        </div>
-        <div className="red" style={{ width: `${red}vw` }}>
-          <span className="text-container">Red Side Win Ratio: {red}%</span>
-        </div>
+        {loading ? (
+          <div>loading</div>
+        ) : (
+          <>
+            <div className="blue" style={{ width: `${blue}vw` }}>
+              <span className="text-container">
+                Blue Side Win Ratio: {blue}%
+              </span>
+            </div>
+            <div className="red" style={{ width: `${red}vw` }}>
+              <span className="text-container">Red Side Win Ratio: {red}%</span>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
