53342b604db0ed1344faca588b751e9039a935dd
kgawerda <konradgawerda@gmail.com>
2023-06-13 18:04:35 +0200

    create home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,14 @@
 import "../styles/Home.css";
 
 const Home = () => {
-  return <></>;
+  return (
+    <>
+      <p className="witam">
+        Witaj przywoływaczu na naszej stronie z danymi LCK Summer 2022!
+      </p>
+      <div className="main witam-grafika"></div>
+    </>
+  );
 };
 
 export default Home;
